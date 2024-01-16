af955400958c4d607ca3dcbfdc603ce630fe44e9
aamir9185 <mohammadaamir9185@gmail.com>
2024-01-16 22:21:49 +0530

    Functionality Added

--- main.js
+++ main.js
@@ -7,6 +7,32 @@ class BankAccount{
    this.accountno = accno;
    this.balance = bal;
 }
+printInfo(){
+   console.log(`Your Name ${this.name} \nYour Account No ${this.accountno} \nYour Current Balance ${this.balance} \n`)
+}
+depositeAmount(amount){
+   if(amount > 0){
+     this.balance += amount;
+     console.log(`Your current balance is ${this.balance} \n`);
+   }else{
+      console.log("Enter a valid amount");
+   }
+}
+
+withdrawAmount(amount) {
+   if (amount > 0 && amount <= this.balance) {
+     this.balance -= amount;
+     console.log(`Withdrawal of ${amount} successful.`);
+   } else {
+     console.log('Invalid withdrawal amount or insufficient funds.');
+   }
+ }
+
+
+
 }
 const P1 = new BankAccount("Aamir",12342421,1000);
-console.log(P1);
+P1.printInfo();
+P1.depositeAmount(1000);
+P1.withdrawAmount(500);
+P1.withdrawAmount(10000)
